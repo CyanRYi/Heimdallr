7333fc181cb3754c790f2b47a44d1b8c4701960f
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2019-07-09 17:04:11 +0900

    JSONUsernamePasswordAuthenticationFilter worked. but need to review SecurityJackson2Modules and Deserializer

--- src/main/java/tech/sollabs/heimdallr/filter/JSONUsernamePasswordAuthenticationFilter.java
+++ src/main/java/tech/sollabs/heimdallr/filter/JSONUsernamePasswordAuthenticationFilter.java
@@ -3,6 +3,7 @@ package tech.sollabs.heimdallr.filter;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -15,7 +16,7 @@ import java.io.IOException;
 /**
  * Convert from JSON login request to UsernamePasswordAuthenticationToken
  *
- * TODO : check SecurityJackson2Modules and UsernamePasswordAuthenticationTokenDeserializer
+ * TODO : check {@link org.springframework.security.jackson2.SecurityJackson2Modules} and UsernamePasswordAuthenticationTokenDeserializer
  *
  * @author Cyan Raphael Yi
  * @since 0.2
@@ -25,13 +26,25 @@ import java.io.IOException;
 public class JSONUsernamePasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     @Override
+    protected boolean requiresAuthentication(HttpServletRequest request, HttpServletResponse response) {
+        return MediaType.APPLICATION_JSON.isCompatibleWith(MediaType.parseMediaType(request.getContentType()));
+    }
+
+    @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) {
 
         try {
-            JSONObject loginRequest = (JSONObject) new JSONParser().parse(request.getReader());
+            JSONObject loginRequest;
 
-            Authentication token = new UsernamePasswordAuthenticationToken(
+            if (request.getContentLength() > 0) {
+                loginRequest = (JSONObject) new JSONParser().parse(request.getReader());
+            } else {
+                loginRequest = new JSONObject();
+            }
+
+            UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(
                     loginRequest.get(getUsernameParameter()), loginRequest.get(getPasswordParameter()));
+            setDetails(request, token);
 
             return getAuthenticationManager().authenticate(token);
         } catch (ParseException | IOException e) {
---
+++ src/test/java/tech/sollabs/heimdallr/filter/JSONUsernamePasswordAuthenticationFilterTests.java
@@ -0,0 +1,176 @@
+package tech.sollabs.heimdallr.filter;
+
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+
+import javax.servlet.ServletException;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+/**
+ * Tests {@link JSONUsernamePasswordAuthenticationFilter}
+ *
+ * This tests extends {@link org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilterTests}
+ *
+ * @author Cyan Raphael Yi
+ * @since 0.3
+ */
+public class JSONUsernamePasswordAuthenticationFilterTests {
+
+    @Test
+    public void testNormalOperation() throws Exception {
+        String content = String.format("{\"%s\": \"%s\", \"%s\": \"%s\"}",
+                JSONUsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, "Cyan",
+                JSONUsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY, "Raphael Yi");
+
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/");
+        request.setContent(content.getBytes());
+        request.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        JSONUsernamePasswordAuthenticationFilter filter = new JSONUsernamePasswordAuthenticationFilter();
+        filter.setAuthenticationManager(createAuthenticationManager());
+
+        Authentication result = filter.attemptAuthentication(request, new MockHttpServletResponse());
+        assertNotNull(result);
+        assertEquals(((WebAuthenticationDetails) result.getDetails()).getRemoteAddress(), "127.0.0.1");
+    }
+
+    @Test
+    public void testNullPasswordHandledGracefully() throws Exception {
+        String content = String.format("{\"%s\": \"%s\"}",
+                JSONUsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, "Cyan");
+
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/");
+        request.setContent(content.getBytes());
+        request.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        JSONUsernamePasswordAuthenticationFilter filter = new JSONUsernamePasswordAuthenticationFilter();
+        filter.setAuthenticationManager(createAuthenticationManager());
+        assertNotNull(filter.attemptAuthentication(request, new MockHttpServletResponse()));
+    }
+
+    @Test
+    public void testNullUsernameHandledGracefully() throws Exception {
+        String content = String.format("{\"%s\": \"%s\"}",
+                JSONUsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY, "Raphael Yi");
+
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/");
+        request.setContent(content.getBytes());
+        request.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        JSONUsernamePasswordAuthenticationFilter filter = new JSONUsernamePasswordAuthenticationFilter();
+        filter.setAuthenticationManager(createAuthenticationManager());
+        assertNotNull(filter.attemptAuthentication(request, new MockHttpServletResponse()));
+    }
+
+    @Test
+    public void testUsingDifferentParameterNamesWorksAsExpected() throws ServletException {
+        String content = String.format("{\"%s\": \"%s\", \"%s\": \"%s\"}",
+                "x", "Cyan",
+                "y", "Raphael Yi");
+
+        JSONUsernamePasswordAuthenticationFilter filter = new JSONUsernamePasswordAuthenticationFilter();
+        filter.setAuthenticationManager(createAuthenticationManager());
+        filter.setUsernameParameter("x");
+        filter.setPasswordParameter("y");
+
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/");
+        request.setContent(content.getBytes());
+        request.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        Authentication result = filter.attemptAuthentication(request, new MockHttpServletResponse());
+        assertNotNull(result);
+        assertEquals(((WebAuthenticationDetails) result.getDetails()).getRemoteAddress(), "127.0.0.1");
+    }
+
+    @Test
+    public void testSpacesAreTrimmedCorrectlyFromUsername() throws Exception {
+        String content = String.format("{\"%s\": \"%s\", \"%s\": \"%s\"}",
+                JSONUsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, "Cyan",
+                JSONUsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY, "Raphael Yi");
+
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/");
+        request.setContent(content.getBytes());
+
+        JSONUsernamePasswordAuthenticationFilter filter = new JSONUsernamePasswordAuthenticationFilter();
+        filter.setAuthenticationManager(createAuthenticationManager());
+
+        Authentication result = filter.attemptAuthentication(request, new MockHttpServletResponse());
+        assertEquals(result.getName(), "Cyan");
+    }
+
+    @Test(expected = AuthenticationException.class)
+    public void testFailedAuthenticationThrowsException() {
+        String content = String.format("{\"%s\": \"%s\"}",
+                JSONUsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY, "Cyan");
+
+        MockHttpServletRequest request = new MockHttpServletRequest("POST", "/");
+        request.setContent(content.getBytes());
+        request.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        JSONUsernamePasswordAuthenticationFilter filter = new JSONUsernamePasswordAuthenticationFilter();
+        AuthenticationManager am = mock(AuthenticationManager.class);
+        when(am.authenticate(any(Authentication.class))).thenThrow(
+                new BadCredentialsException(""));
+        filter.setAuthenticationManager(am);
+
+        filter.attemptAuthentication(request, new MockHttpServletResponse());
+    }
+
+    /**
+     * SEC-571
+     */
+    @Test
+    public void noSessionIsCreatedIfAllowSessionCreationIsFalse() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod("POST");
+        request.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+        JSONUsernamePasswordAuthenticationFilter filter = new JSONUsernamePasswordAuthenticationFilter();
+        filter.setAllowSessionCreation(false);
+        filter.setAuthenticationManager(createAuthenticationManager());
+
+        filter.attemptAuthentication(request, new MockHttpServletResponse());
+
+        assertNull(request.getSession(false));
+    }
+
+    @Test
+    public void passedByFormLoginRequest() throws Exception {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        request.setMethod("POST");
+        request.setContentType(MediaType.APPLICATION_FORM_URLENCODED_VALUE);
+        request.addParameter(JSONUsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY,"Cyan");
+        request.addParameter(JSONUsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY,"Raphael Yi");
+
+        JSONUsernamePasswordAuthenticationFilter filter = new JSONUsernamePasswordAuthenticationFilter();
+        filter.setAuthenticationManager(createAuthenticationManager());
+
+        assertFalse(filter.requiresAuthentication(request, new MockHttpServletResponse()));
+    }
+
+    private AuthenticationManager createAuthenticationManager() {
+        AuthenticationManager am = mock(AuthenticationManager.class);
+        when(am.authenticate(any(Authentication.class))).thenAnswer(
+                new Answer<Authentication>() {
+                    public Authentication answer(InvocationOnMock invocation)
+                            throws Throwable {
+                        return (Authentication) invocation.getArguments()[0];
+                    }
+                });
+
+        return am;
+    }
+}
