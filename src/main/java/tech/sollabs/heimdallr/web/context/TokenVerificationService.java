071dd133580c6beeb32dcd95218e30e0228e7669
Cyan Raphael Yi <cyan.r.yi@gmail.com>
2019-07-15 21:55:06 +0900

    change throw declation to InvalidTokenException and adds javadoc

--- src/main/java/tech/sollabs/heimdallr/web/context/TokenVerificationService.java
+++ src/main/java/tech/sollabs/heimdallr/web/context/TokenVerificationService.java
@@ -1,9 +1,13 @@
 package tech.sollabs.heimdallr.web.context;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
+import tech.sollabs.heimdallr.exception.InvalidTokenException;
 
+/**
+ * Create Authentication from valid token
+ * when Token is Invalid, throw {@link InvalidTokenException}
+ */
 public interface TokenVerificationService {
 
-    Authentication verifyToken(String token) throws AuthenticationException;
+    Authentication verifyToken(String token) throws InvalidTokenException;
 }
